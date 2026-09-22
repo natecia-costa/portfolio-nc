f54e24ba27ce4678810cadb92defca7b61b2ee02
Natécia Costa <natecia_ilha@hotmail.com>
2026-09-22 09:45:05 -0300

    ajustes de telas pequenas

--- src/components/Contato/BotaoContato.jsx
+++ src/components/Contato/BotaoContato.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function BotaoContato({ className, href, externo = true, icon,conteudo }) {
   return (
       <a
-        className={`botao-conteudo ${className || ""}`}
+        className={`botao-contato ${className || ""}`}
         href={href}
         target={externo ? "_blank" : undefined}
         rel={externo ? "noopener noreferrer" : undefined}
